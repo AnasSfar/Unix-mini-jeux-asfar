79caef31e8ca18b0065ac626199d9876a229acbc
AnasSfar <sfaranas13@gmail.com>
2025-10-28 19:37:55 +0100

    Update .remise_zero.sh

--- portes/Feu/.remise_zero.sh
+++ portes/Feu/.remise_zero.sh
@@ -1,1 +1,13 @@
+if [[ -f .start_time ]]; then
+    rm .start_time
+fi
 
+if [[ -f .final_time ]]; then
+    rm .final_time
+fi
+
+if [[ -f .module_OK ]]; then
+    rm .module_OK
+fi
+
+echo "♻️  La porte du Feu a été remise à zéro."
