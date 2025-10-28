d4123b5fc952df57a9cd94c1882765078025cd73
AnasSfar <sfaranas13@gmail.com>
2025-10-28 19:38:47 +0100

    Update .rep.sh

--- portes/Feu/.rep.sh
+++ portes/Feu/.rep.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Usage: .rep.sh reponse_joueur bonne_reponse
 
+reponse=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+bonne=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+
+if [[ "$reponse" == "$bonne" ]]; then
+    echo "✔️  Bonne réponse !"
+    exit 0
+else
+    echo "❌  Mauvaise réponse..."
+    exit 1
+fi
