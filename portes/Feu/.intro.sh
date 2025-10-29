43ea479bfdbaa63b03c4e82fdf9eb7b819655be3
AnasSfar <sfaranas13@gmail.com>
2025-10-29 16:17:34 +0100

    Update .intro.sh
    added more details

--- portes/Feu/.intro.sh
+++ portes/Feu/.intro.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo ""
-echo "🔥==============================🔥"
-echo "     PORTE DU FEU — ÉPREUVE 1"
-echo "🔥==============================🔥"
+echo "     🔥 PORTE DU FEU — ÉPREUVE 1 🔥   "
 echo ""
 echo "Le feu teste ta rapidité et ton instinct."
 echo "Réponds par 'Vrai' ou 'Faux' à chaque affirmation."
 echo "Tape OUT si tu veux abandonner la flamme..."
-echo ""
+echo "Attention, le nombre de bonne réponse et le temps de l'épreuve évalueront ton équilibre du feu 🔥"
